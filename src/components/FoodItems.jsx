5ba7a666105179652dc1de563b1f69feed6cf5cb
Sarvesh Kumar <137796894+Sarvesh2024@users.noreply.github.com>
2025-12-26 00:52:03 +0530

    chore: commit after removing stale index.lock

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -4,16 +4,21 @@ import Item from './Item';
 
 const FoodItems = ({items}) =>{
    let [activeItems,setActiveItems] = useState([]);
+
+   let onBuyButton = (item) =>{
+      let newItems = [...activeItems,item];
+      setActiveItems(newItems);
+   }
     return(
    
      <ul className="list-group">
         {items.map((item) => (
            <Item key= {item} foodItem={item}
             bought={activeItems.includes(item)}
-           handleBuyButton = {() =>console.log(`${item} bought`)}/>
+           handleBuyButton ={(event)=>onBuyButton(item,event)}/>
         ))}
       </ul>
    );
 }
 
-export default FoodItems;git
+export default FoodItems;
